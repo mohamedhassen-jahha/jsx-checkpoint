608a5b4522e623524c0efc0d33735be5327e9cf6
Mohamed Hassen JAHHA <mohamedhassen.jahha@outlook.fr>
2021-10-18 00:49:43 +0200

    Up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import imageInSrc from "./imageInSrc.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div
+        style={{ border: "solid 1px black", maxWidth: "100vw" }}
+        className="submain"
+      >
+        <h1 className="title red">Squid game</h1>
+        <br />
+        <img src={imageInSrc} alt="pic" />
+        <br />
+        <img src="/imageInPublic.jpg" alt="pic2" />
+      </div>
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
